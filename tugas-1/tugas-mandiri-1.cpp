32b9f6d1852da6241655165dae0c7142a066079c
ridwannurvauzi <147250493+ridwannurvauzi@users.noreply.github.com>
2023-11-12 12:25:07 +0700

    Add files via upload

---
+++ tugas-1/tugas-mandiri-1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  char nim[10] = "2341079";
+  char nama[30] = "RIDWAN NURVAUZI";
+ char jurusan[30] = "S-1 Informatika ";
+
+    printf("NIM: %s\n", nim);
+    printf("NAMA: %s\n", nama);
+    printf("JURUSAN: %s\n", jurusan);
+
+    return 0;
+	}
